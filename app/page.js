d9573fe17e63a1bd80af2afb0848e2fd75b3e83e
saravanan <saransri13@gmail.com>
2026-06-04 00:12:48 +0530

    Fix: set revalidate=0 for live Strapi data

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import HomeContent from "@/components/HomeContent";
 import { getHomePageData } from "@/lib/strapi/queries";
 
-export const revalidate = 60;
+export const revalidate = 0;
 
 export default async function HomePage() {
   const data = await getHomePageData();
